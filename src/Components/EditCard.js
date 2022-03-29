14bb451e6b3e542b85db0e66ce53e44f64e1b517
Bogdan Trivic <62573562+BogdanT258@users.noreply.github.com>
2022-03-29 03:17:12 +0200

    commit

--- src/Components/EditCard.js
+++ src/Components/EditCard.js
@@ -14,7 +14,7 @@ class EditCard extends Component{
         this.state = {
             openModal: false,
             labels: [],
-            data: []
+            data: [{title: "", description: "", labels: []}]
         }
     }
     async componentDidMount() {
@@ -42,22 +42,40 @@ class EditCard extends Component{
     handleChange = e => {
         let obj = {...this.state.data};
         console.log(obj);
-        //   if (e.target.name === "labels") {               
-        //       if (obj.labels.includes(e.target.value)) {
-        //         console.log("Overlaped label");            
-        //       }else{                                 
-        //         obj.labels.push(e.target.value);
-        //         this.setState({
-        //           data: obj
-        //         });
-        //       }        
-        //   }else{
-        //     obj[e.target.name] = e.target.value
-        //     this.setState({
-        //       data: obj
-        //    });
-        //   }      
+          if (e.target.name === "labels") {               
+              if (obj.labels.includes(e.target.value)) {
+                console.log("Overlaped label");            
+              }else{                                 
+                obj[0].labels.push(e.target.value);
+                this.setState({
+                  data: obj
+                });
+              }        
+          }else{
+            obj[0][e.target.name] = e.target.value
+            this.setState({
+              data: obj
+           });
+          }      
       }
+    saveCard = async (e) => {
+        e.preventDefault();
+        try {
+          const url = `http://localhost:3004/cards/${this.props.id}`;
+          console.log("save card");
+          const options = {
+          method: "PUT",
+          headers: {
+            'Accept': 'application/json', 
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(this.state.data[0])         
+        }
+          await(await fetch(url, options)).json();              
+        } catch (err) {
+          console.log(err);
+        }          
+    }      
     render(){
         return(
           <div>
@@ -66,26 +84,20 @@ class EditCard extends Component{
                     </Button>
                     <Modal open={this.state.openModal} onClose={this.onCloseModal}>
                       <Form onSubmit={this.saveCard}>
-                        <Form.Group className="mb-3">
-                            {this.state.data.map(item => {
-                                return(
-                                <div>
+                        <Form.Group className="mb-3">                            
                                     <Form.Label>Title</Form.Label>
-                                    <Form.Control type="text" placeholder="Enter title" name="title" value={item.title} onChange={this.handleChange}/>                                               
-                                </div>
-                                )
-                            })}                          
+                                    <Form.Control type="text" placeholder="Enter title" name="title" value={this.state.data[0].title} onChange={this.handleChange}/>                                                                                                       
                         </Form.Group>
 
                         <Form.Group className="mb-3">
                         <Form.Label>Description</Form.Label>
-                        <Form.Control as="textarea" rows={5} placeholder="Enter description" value={this.state.data.description} onChange={this.handleChange} name="description"/>
+                        <Form.Control as="textarea" rows={5} placeholder="Enter description" value={this.state.data[0].description} onChange={this.handleChange} name="description"/>
                       </Form.Group>
 
                       <Form.Group className="mb-3">
                       <Form.Select aria-label="Default select example" value={this.state.data.labels} onChange={this.handleChange} name='labels'  multiple={true} type="select-multiple">                                              
-                        {this.state.labels.map(item => {                        
-                          return <option key={item} value={item}>{item}</option>                                                                                                             
+                        {this.state.data[0].labels.map(item => {                        
+                          return <option key={item} value={item} selected>{item}</option>                                                                                                             
                         })}                        
                       </Form.Select>
                       </Form.Group>
